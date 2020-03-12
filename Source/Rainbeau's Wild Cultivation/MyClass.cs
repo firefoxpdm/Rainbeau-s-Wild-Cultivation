a527e44cdb57c549665605d9fc0d765a572ea302
Tamas.Szekeres <firefoxpdm@yahoo.co.uk>
2020-03-12 18:23:45 +0100

    Updated to 1.1

--- Source/Rainbeau's Wild Cultivation/MyClass.cs
+++ Source/Rainbeau's Wild Cultivation/MyClass.cs
@@ -1,7 +1,5 @@
-﻿using Harmony;
+﻿using HarmonyLib;
 using RimWorld;
-using System;
-using System.Linq;
 using Verse;
 
 namespace RWC_Code {
@@ -9,7 +7,7 @@ namespace RWC_Code {
 	[StaticConstructorOnStartup]
 	internal static class RWC_Initializer {
 		static RWC_Initializer() {
-			HarmonyInstance harmony = HarmonyInstance.Create("net.rainbeau.rimworld.mod.wildcultivation");
+			Harmony harmony = new Harmony("net.rainbeau.rimworld.mod.wildcultivation");
 			harmony.Patch(AccessTools.Method(typeof(Designator_ZoneAdd_Growing), "CanDesignateCell"), new HarmonyMethod(typeof(Designator_ZoneAdd_Growing_RWC), "CanDesignateCellPrefix"), null);
 			harmony.Patch(AccessTools.Method(typeof(Designator_ZoneAdd_Growing), "CanDesignateCell"), null, new HarmonyMethod(typeof(Designator_ZoneAdd_Growing_RWC), "CanDesignateCellPostfix"));
 			LongEventHandler.QueueLongEvent(Setup, "LibraryStartup", false, null);
